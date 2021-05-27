af751f6a2148e318dfd29615bdfe995b07c7ee60
Jasper Tung <jtunggsp@gmail.com>
2021-05-27 16:59:40 -0700

    Persist redux store

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import React from 'react'
 import Main from './components/MainComponent'
 import { Provider } from 'react-redux'
 import { ConfigureStore } from './redux/configureStore'
+import { PersistGate } from 'redux-persist/es/integration/react'
+import Loading from './components/LoadingComponent'
 
 // creating redux store
-const store = ConfigureStore()
+const { persistor, store } = ConfigureStore()
 
 export default function App() {
   return (
       <Provider store={store}>
         {/* pass store into Provider component to give Main and all its child components ability to connect to redux store */}
-        <Main />
+        <PersistGate
+          loading={<Loading/>}
+          persistor={persistor}
+        >
+          {/* prevents app from rendering until redux store has rehydrated fully from client side storage */}
+          <Main />
+        </PersistGate>
       </Provider>
   )
 }
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -6,10 +6,25 @@ import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist'
+import storage from 'redux-persist/es/storage'
+
+// setting configuration values
+const config = {
+    // 1st property (required) --string value of root
+    key: 'root',
+    // 2nd property (required) --storage object from import (default: local storage)
+    storage,
+    // 3rd property (optional) --causes redux persist to console log messages to debug issues
+    debug: true
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        //combineReducers({
+
+        // now persistCombineReducers handles updating state to local storage whenever reducer is used to update redux store
+        persistCombineReducers(config, {
             campsites,
             comments,
             partners,
@@ -19,7 +34,10 @@ export const ConfigureStore = () => {
         applyMiddleware(thunk, logger)
     );
 
-    return store;
+    const persistor = persistStore(store)
+    // enables store to be persisted and use persistor in app.js
+
+    return { persistor, store }; //add persistor to be able to access both from app.js
 }
 
 // importing redux middlewares thunk and logger
